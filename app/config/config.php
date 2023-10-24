8ba460826bc81857f966ee182a3b719373bc05c3
Syahnan <syhnnzhra@gmail.com>
2023-10-24 18:51:58 +0700

    tugas MVC

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('BASEURL', 'http://localhost/Prakweb2023_C_213040108_MVC/public/');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'prakweb2023_mvc');
+
+
+?>
---
+++ app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_model {
+    private $nama = "Syahnan Afifah";
+
+    public function getUser() {
+        return $this->nama;
+    }
+}
+
+?>
